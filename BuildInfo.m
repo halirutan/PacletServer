b3d1f66c34cf6506fa33b6a8d112dda3edd6c3a4
b3m2a1 <b3m2a1@gmail.com>
2018-06-06 02:29:10 -0700

    Played with theme

--- BuildInfo.m
+++ BuildInfo.m
@@ -1,3 +1,3 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-{"LastBuild" -> DateObject[{2018, 6, 6, 2, 20, 16.972684`7.982325510332142}, 
+{"LastBuild" -> DateObject[{2018, 6, 6, 2, 28, 54.069129`8.485524348010237}, 
    "Instant", "Gregorian", -7.]}
